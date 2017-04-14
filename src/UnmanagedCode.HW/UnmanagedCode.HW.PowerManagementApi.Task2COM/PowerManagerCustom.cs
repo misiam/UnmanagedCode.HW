8d3c0bca902c856612312154397e98eb773dd9ba
Mikhail_Marykin <Mikhail_Marykin@epam.com>
2017-04-14 19:22:15 +0300

    create project for
    Задание 2.
    На основе данной библиотеки создайте COM компонент, который будет доступен из скриптовых языков и VBA (с поддержкой IDispatch)

---
+++ src/UnmanagedCode.HW/UnmanagedCode.HW.PowerManagementApi.Task2COM/PowerManagerCustom.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnmanagedCode.HW.PowerManagementApi.Task1;
+
+namespace UnmanagedCode.HW.PowerManagementApi.Task2COM
+{
+    public class PowerManagerCustom
+    {
+        private PowerManager _powerManager = new PowerManager();
+        private SuspendManager _suspendManager = new SuspendManager();
+        private HibernateFileManager _hibernateFileManager = new HibernateFileManager();
+        
+        
+         
+    }
+}
